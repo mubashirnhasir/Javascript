1015d3511b984e73db130b3e972d84daee6cce05
mubashirnhasir <127984059+mubashirnhasir@users.noreply.github.com>
2024-08-29 15:51:51 +0000

    Variables Commit

--- JS/Test.js
+++ JS/Test.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+const myName = "Mubashir Nisar" // Cannot be changes if declared once
+let emailId = "mubashirnhasir@gmail.com" //this is a block space variable and cannot be redeclared in same block
+var password = "123123123" // can be easily changed everywhere in your code
+let newValue
+
+console.table([myName, emailId, password, newValue])
